5864b70ce27107d417669302f20bb97386d170ba
Georgiy.Kutasov <Georgiy.Kutasov@tatar.ru>
2026-04-01 18:08:24 +0300

    Добавлена подпись и индексы для компонента заголовок

--- SimpleUI/Header.cs
+++ SimpleUI/Header.cs
@@ -2,17 +2,22 @@ using Android.Content;
 using Android.Runtime;
 using Android.Util;
 using Android.Views;
+using static Android.Preferences.PreferenceActivity;
 
 namespace SimpleUI
 {
     public class Header : LinearLayout
     {
+        private TextView header;
+        private TextView subheader;
 
         public Header(Context? context, IAttributeSet? attrs) : base(context, attrs)
         {
+            //header = FindViewById<TextView>(Resource.Id.headerText);
+
             LayoutInflater inflater;
             inflater = (LayoutInflater)context.GetSystemService(Context.LayoutInflaterService);
-            inflater.Inflate(Resource.Layout.Header2, this, true);
+            inflater.Inflate(Resource.Layout.header_layout, this, true);
         }
     }
 }
