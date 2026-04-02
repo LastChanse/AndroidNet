00ee29d31f8001a2fb53f715133475900b670112
Georgiy.Kutasov <Georgiy.Kutasov@tatar.ru>
2026-04-02 10:43:06 +0300

    Добавление специальных атрибутов для виджета

--- SimpleUI/Header.cs
+++ SimpleUI/Header.cs
@@ -1,8 +1,9 @@
 using Android.Content;
-using Android.Runtime;
+using Android.Content.Res;
+using Android.Graphics.Drawables;
+using Android.Media;
 using Android.Util;
 using Android.Views;
-using static Android.Preferences.PreferenceActivity;
 
 namespace SimpleUI
 {
@@ -14,11 +15,26 @@ namespace SimpleUI
 
         public Header(Context? context, IAttributeSet? attrs) : base(context, attrs)
         {
-            //header = FindViewById<TextView>(Resource.Id.headerText);
-
+            // Загружаем макет
             LayoutInflater inflater;
             inflater = (LayoutInflater)context.GetSystemService(Context.LayoutInflaterService);
             inflater.Inflate(Resource.Layout.header_layout, this, true);
+
+            // Получаем элементы макета
+            header = FindViewById<TextView>(Resource.Id.headerText);
+            subheader = FindViewById<TextView>(Resource.Id.subheaderText);
+            headerImage = FindViewById<ImageView>(Resource.Id.headerImage);
+
+            // Получаем атрибуты
+            TypedArray custom_attrs = context.Theme.ObtainStyledAttributes(attrs, Resource.Styleable.Header, 0,0);
+            String header_text = custom_attrs.GetString(Resource.Styleable.Header_headerText);
+            String subheader_text = custom_attrs.GetString(Resource.Styleable.Header_subheaderText);
+            int image = custom_attrs.GetResourceId(Resource.Styleable.Header_image, Resource.Drawable.default_img);
+            
+            // Применяем атрибуты
+            header.Text = header_text != null ? header_text : "Header";
+            subheader.Text = subheader_text != null ? subheader_text : "Subeader";
+            headerImage.SetImageResource(image);
         }
     }
 }
